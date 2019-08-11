e61166b623e7902b9a2ee94f2992e70071e07ee8
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2019-08-11 11:26:30 -0500

    display courses using spinner

--- app/src/main/java/io/github/vanessa85/notekeeper/NoteKeeperData.kt
+++ app/src/main/java/io/github/vanessa85/notekeeper/NoteKeeperData.kt
@@ -1,5 +1,9 @@
 package io.github.vanessa85.notekeeper
 
-class CourseInfo(val courseId: String, val title: String)
+class CourseInfo(val courseId: String, val title: String) {
+    override fun toString(): String {
+        return title
+    }
+}
 
 class NoteInfo(var course: CourseInfo, var title: String, var text: String)
