c747fd3d305999176969bbb1544826bfd7764c46
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2019-08-11 20:37:40 -0500

    singleton and display note

---
+++ app/src/main/java/io/github/vanessa85/notekeeper/Constants.kt
@@ -0,0 +1,4 @@
+package io.github.vanessa85.notekeeper
+
+const val EXTRA_NOTE_POSITION = "EXTRA_NOTE_POSITION"
+const val POSITION_NOT_SET = -1
--- app/src/main/java/io/github/vanessa85/notekeeper/MainActivity.kt
+++ app/src/main/java/io/github/vanessa85/notekeeper/MainActivity.kt
@@ -1,7 +1,6 @@
 package io.github.vanessa85.notekeeper
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
@@ -11,18 +10,33 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var notePosition = POSITION_NOT_SET
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        val dm = DataManager()
         val adapterCourses = ArrayAdapter<CourseInfo>(this, android.R.layout.simple_spinner_item,
-            dm.courses.values.toList())
+            DataManager.courses.values.toList())
         adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         spinnerCourses.adapter = adapterCourses
+
+        notePosition = intent.getIntExtra(EXTRA_NOTE_POSITION, POSITION_NOT_SET)
+
+        if (notePosition != POSITION_NOT_SET)
+            displayNote()
+    }
+
+    private fun displayNote() {
+        val note = DataManager.notes[notePosition]
+        textNoteTitle.setText(note.title)
+        textNoteText.setText(note.text)
+
+        val coursePosition = DataManager.courses.values.indexOf(note.course)
+        spinnerCourses.setSelection(coursePosition)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
