213729e5b34e33b03f1e80fe3cd79e99a7600631
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2019-08-11 19:26:42 -0500

    added notelist activity

---
+++ app/src/main/java/io/github/vanessa85/notekeeper/NoteListActivity.kt
@@ -0,0 +1,23 @@
+package io.github.vanessa85.notekeeper
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_note_list.*
+
+class NoteListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_list)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            var activityIntent = Intent(this, MainActivity::class.java)
+            startActivity(activityIntent)
+        }
+    }
+
+}
