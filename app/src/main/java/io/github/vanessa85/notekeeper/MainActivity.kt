f464bf97c82df510c6d437ac1c76a6368d75e134
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2019-08-13 06:44:51 -0500

    saved note position

--- app/src/main/java/io/github/vanessa85/notekeeper/MainActivity.kt
+++ app/src/main/java/io/github/vanessa85/notekeeper/MainActivity.kt
@@ -1,6 +1,7 @@
 package io.github.vanessa85.notekeeper
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
@@ -23,7 +24,8 @@ class MainActivity : AppCompatActivity() {
 
         spinnerCourses.adapter = adapterCourses
 
-        notePosition = intent.getIntExtra(EXTRA_NOTE_POSITION, POSITION_NOT_SET)
+        notePosition = savedInstanceState?.getInt(NOTE_POSITION, POSITION_NOT_SET) ?:
+            intent.getIntExtra(NOTE_POSITION, POSITION_NOT_SET)
 
         if (notePosition != POSITION_NOT_SET)
             displayNote()
@@ -94,5 +96,8 @@ class MainActivity : AppCompatActivity() {
         note.course = spinnerCourses.selectedItem as CourseInfo
     }
 
-
+    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState?.putInt(NOTE_POSITION, notePosition)
+    }
 }
